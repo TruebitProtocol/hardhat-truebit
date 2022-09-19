f2f5f0a0f36ccf5b15343cdeefdb4967c605e6d0
matias.lareo <matias.lareo@fortegrp.com>
2022-09-19 14:12:53 -0300

    fix: console info on task submission

--- truebit.js
+++ truebit.js
@@ -363,7 +363,7 @@ task("task", "List all task submitted on TruebitOS")
                 if(taskArgs.a || taskArgs.f){
                     console.log("Submitting task to blockchain...")
                     const message = await submitTask({account: parseInt(taskArgs.a),taskFile: taskArgs.f});
-                    console.info(message);
+                    console.info(`${message.data.message} with hash ${message.data.hash}`);
                 }
                 else{
                     console.info("Provide account and file arguments")
